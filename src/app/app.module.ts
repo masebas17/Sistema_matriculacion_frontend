b27c306bbc0527be4cb29e0670fedd157d55f45b
Mario <masebas17@hotmail.com>
2022-08-13 01:19:02 -0500

    actualización componentes y pantallas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponentComponent } from './header-component/header-component.component';
+import { FooterComponentComponent } from './footer-component/footer-component.component';
+import { MainMenuComponent } from './main-menu/main-menu.component';
+import { SearcherComponent } from './searcher/searcher.component';
+import { ScheduleSelectionComponent } from './schedule-selection/schedule-selection.component';
+import { CourseSelectionComponent } from './course-selection/course-selection.component';
+import { EnrollmentFormComponent } from './enrollment-form/enrollment-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponentComponent,
+    FooterComponentComponent,
+    MainMenuComponent,
+    SearcherComponent,
+    ScheduleSelectionComponent,
+    CourseSelectionComponent,
+    EnrollmentFormComponent,
   ],
   imports: [
     BrowserModule,
