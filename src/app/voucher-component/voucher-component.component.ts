328dda164752b89495457e3f0d585f3b9fc40d7f
Mario <masebas17@hotmail.com>
2022-09-06 23:24:41 -0500

    correcion errores pdf descargas

--- src/app/voucher-component/voucher-component.component.ts
+++ src/app/voucher-component/voucher-component.component.ts
@@ -1,7 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-declare var jsPDF: any;
-import html2canvas from 'html2canvas';
-import { position } from 'html2canvas/dist/types/css/property-descriptors/position';
+
 
 
 
@@ -10,40 +8,15 @@ import { position } from 'html2canvas/dist/types/css/property-descriptors/positi
   templateUrl: './voucher-component.component.html',
   styleUrls: ['./voucher-component.component.css']
 })
+
 export class VoucherComponentComponent implements OnInit {
 
  
   constructor() { }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(){}
 
-  downloadPDF() {
-    const DATA: any = document.querySelector("#htmlData");
-    const doc = new jsPDF('p','mm','a4');
-    const options = {
-      background: 'white',
-      scale: 4
-    };
-    
-    html2canvas(DATA, options).then((canvas) => {
-
-      const img = canvas.toDataURL("image/png");
-      const pdfwidth = canvas.width/20;
-      const pdfheight = (canvas.height * pdfwidth) / canvas.width;
-
-      console.log(canvas.height)
-      console.log(canvas.width)
-      console.log(pdfwidth)
-
-      doc.addImage(img, 'png', 0, 0, pdfwidth, pdfheight, undefined, 'FAST');
-      
-      return doc;
-    
-   }).then((docResult) => {
-    docResult.save(`${new Date().toISOString()}_tutorial.pdf`);
-  });
-  }
+  
 }
 
 
