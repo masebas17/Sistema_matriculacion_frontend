5c27daa0862dd63f254c7d40e3d41286e08f5dbd
Mario Sebastián Andrade <95883359+masebas17@users.noreply.github.com>
2024-08-22 01:41:20 -0500

    fix swal modal of course_selection

--- src/app/course-selection/course-selection.component.ts
+++ src/app/course-selection/course-selection.component.ts
@@ -7,65 +7,75 @@ import Swal from 'sweetalert2';
 @Component({
   selector: 'app-course-selection',
   templateUrl: './course-selection.component.html',
-  styleUrls: ['./course-selection.component.css']
+  styleUrls: ['./course-selection.component.css'],
 })
 export class CourseSelectionComponent implements OnInit {
-  
   tittle = 'client';
   course: any;
   sheduleId: number = 0;
   course_selection: any;
-  
 
-  constructor(private _apiService:ApiService,
+  constructor(
+    private _apiService: ApiService,
     private _router: Router,
-    private activateRoute: ActivatedRoute ) {
-    this.course = {}
-   }
+    private activateRoute: ActivatedRoute
+  ) {
+    this.course = {};
+  }
 
   ngOnInit(): void {
-    this.activateRoute.params.subscribe( params =>{
-      console.log(parseInt(params['id']))
-      this.sheduleId = parseInt(params['id'])
-    })
-    this.getCourse()
+    this.activateRoute.params.subscribe((params) => {
+      console.log(parseInt(params['id']));
+      this.sheduleId = parseInt(params['id']);
+    });
+    this.getCourse();
     setTimeout(() => {
       this.anunce();
     }, 1000);
   }
 
-  getCourse(){
-    this._apiService.getCourses(this.sheduleId).subscribe((resp: datacourses[]) =>
-    {
-      console.log(resp),
-      this.course = resp
-    })
+  getCourse() {
+    this._apiService
+      .getCourses(this.sheduleId)
+      .subscribe((resp: datacourses[]) => {
+        console.log(resp), (this.course = resp);
+      });
   }
 
-  enroll(event: any){
-    console.log(event.target.name)
-    localStorage.setItem("en", event.target.name)
-    this._router.navigate(['/enrollment', event.target.name])
-    
+  enroll(event: any) {
+    console.log(event.target.name);
+    localStorage.setItem('en', event.target.name);
+    this._router.navigate(['/enrollment', event.target.name]);
   }
 
-  async anunce(){
-   await Swal.fire({
+  async anunce() {
+    await Swal.fire({
       icon: 'info',
-      text: 'Estimado alumno/a, a continuación el proceso nos muestra los cursos disponibles para matricularnos, es importante que conozcas que el curso que se escoja al finalizar el proceso puede cambiar, esto es debido a que el sistema verifica al terminar las matrículas parámetros como la edad o si existe alguna limitante de imprevisto como el espacio físico.',
+      title: 'Atención',
+      text: 'Estimados catequizandos y padres de familia, a continuación el proceso de registro nos muestra los cursos disponibles para matricularnos, es importante que tengamos en cuenta que el curso que se escoja al finalizar el proceso podría cambiar, esto es debido a que el sistema verifica al terminar las matrículas parámetros como la edad o si existe alguna limitante de imprevisto con el espacio físico. Agradecemos su comprensión.',
       confirmButtonColor: '#1D71B8',
-      input: "checkbox",
+      input: 'checkbox',
       inputValue: 1,
       inputPlaceholder: `
         Estoy de acuerdo con los términos y condiciones
       `,
       confirmButtonText: `
-        Continue&nbsp;<i class="fa fa-arrow-right"></i>
+        Continuar&nbsp;<i class="fa fa-arrow-right"></i>
       `,
       inputValidator: (result) => {
-        return !result && "You need to agree with T&C";
-      }
+        return !result && 'You need to agree with T&C';
+      },
+      allowOutsideClick: () => {
+        const popup = Swal.getPopup() as HTMLElement;
+        popup.classList.remove('swal2-show');
+        setTimeout(() => {
+          popup.classList.add('animate__animated', 'animate__headShake');
+        });
+        setTimeout(() => {
+          popup.classList.remove('animate__animated', 'animate__headShake');
+        }, 500);
+        return false;
+      },
     });
   }
-
 }
